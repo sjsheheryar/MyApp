48eb7356d39e5e17ada48cec579e7698c7c82ee8
sjsheheryar <syed.sheheryar@gmail.com>
2023-11-02 17:43:59 +0000

    Name changed.

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello Sheheryar");
+            Console.Write("Hello Syed Sheheryar");
         }
     }
 }
